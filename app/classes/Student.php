ffaad89c5b257d037343c2191ed37d6e9d8cf62d
Zillur Imran <zillurimran@gmail.com>
2022-11-19 04:34:58 +0600

    data added to table dynamically

---
+++ app/classes/Student.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\classes;
+
+class Student
+{
+    public function student(){
+        return [
+            [
+                'id'=>1,
+                "name"=>'Salam',
+                "email"=>'salam@gmail.com',
+                "address"=> 'Dhaka',
+                "phone"=>"345678"
+            ],
+            [
+                'id'=>2,
+                "name"=>'Barkat',
+                "email"=>'barkat@gmail.com',
+                "address"=> 'Dhaka',
+                "phone"=>"345678"
+            ],
+            [
+                'id'=>3,
+                "name"=>'Rafiq',
+                "email"=>'rafiq@gmail.com',
+                "address"=> 'Dhaka',
+                "phone"=>"345678"
+            ],
+            [
+                'id'=>4,
+                "name"=>'Jabbar',
+                "email"=>'jabbar@gmail.com',
+                "address"=> 'Dhaka',
+                "phone"=>"345678"
+            ],
+            [
+                'id'=>5,
+                "name"=>'Asad',
+                "email"=>'asad@gmail.com',
+                "address"=> 'Dhaka',
+                "phone"=>"345678"
+            ],
+            [
+                'id'=>6,
+                "name"=>'Rahim',
+                "email"=>'rahim@gmail.com',
+                "address"=> 'Dhaka',
+                "phone"=>"345678"
+            ],
+            [
+                'id'=>7,
+                "name"=>'Karim',
+                "email"=>'karim@gmail.com',
+                "address"=> 'Dhaka',
+                "phone"=>"345678"
+            ],
+        ];
+    }
+
+}
