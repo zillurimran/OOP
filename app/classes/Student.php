fa2475d1ed26d6168b6f82b02b5127a04a5e286c
Zillur Imran <zillurimran@gmail.com>
2022-11-19 20:29:56 +0600

    added row for multiple phone

--- app/classes/Student.php
+++ app/classes/Student.php
@@ -10,49 +10,70 @@ class Student
                 "name"=>'Salam',
                 "email"=>'salam@gmail.com',
                 "address"=> 'Dhaka',
-                "phone"=>"345678"
+                "phone"=>[
+                    'Phone_1'=>"345678",
+                    'Phone_2'=>"587867986"
+                   ]
             ],
             [
                 'id'=>2,
                 "name"=>'Barkat',
                 "email"=>'barkat@gmail.com',
                 "address"=> 'Dhaka',
-                "phone"=>"345678"
+                "phone"=>[
+                    'Phone_1'=>"86767865",
+                    'Phone_2'=>"78676764"
+                ]
             ],
             [
                 'id'=>3,
                 "name"=>'Rafiq',
                 "email"=>'rafiq@gmail.com',
                 "address"=> 'Dhaka',
-                "phone"=>"345678"
+                "phone"=>[
+                    'Phone_1'=>"566898843",
+                    'Phone_2'=>"1298759893"
+                ]
             ],
             [
                 'id'=>4,
                 "name"=>'Jabbar',
                 "email"=>'jabbar@gmail.com',
                 "address"=> 'Dhaka',
-                "phone"=>"345678"
+                "phone"=>[
+                    'Phone_1'=>"767689765",
+                    'Phone_2'=>"0998663434"
+                ]
             ],
             [
                 'id'=>5,
                 "name"=>'Asad',
                 "email"=>'asad@gmail.com',
                 "address"=> 'Dhaka',
-                "phone"=>"345678"
+                "phone"=>[
+                    'Phone_1'=>"143356677",
+                    'Phone_2'=>"07967676"
+                ]
             ],
             [
                 'id'=>6,
                 "name"=>'Rahim',
                 "email"=>'rahim@gmail.com',
                 "address"=> 'Dhaka',
-                "phone"=>"345678"
+                "phone"=>[
+                    'Phone_1'=>"3665488",
+                    'Phone_2'=>"9786565"
+                ]
             ],
             [
                 'id'=>7,
                 "name"=>'Karim',
                 "email"=>'karim@gmail.com',
                 "address"=> 'Dhaka',
-                "phone"=>"345678"
+                "phone"=>[
+                    'Phone_1'=>"767678",
+                    'Phone_2'=>"989877"
+                ]
             ],
         ];
     }
